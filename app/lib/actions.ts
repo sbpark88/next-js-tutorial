67980f1598808e34bbbacf517e3c60a0e81112c5
sbpark88 <saebyulpark88@gmail.com>
2024-06-19 20:23:38 +0900

    Add: Create Server Action

---
+++ app/lib/actions.ts
@@ -0,0 +1,27 @@
+'use server';
+
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  // input type="number" 가 실제로는 number 가 아닌 string 을 반환하기 때문에 z.number() 를 사용하면 에러 발생
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  const rawFormData = Object.fromEntries(formData.entries());
+  const { customerId, amount, status } = CreateInvoice.parse(rawFormData);
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+}
