4b672ce7ee5ebdacbb70358f9cbaecc5c583e3b2
sbpark88 <saebyulpark88@gmail.com>
2024-06-19 15:56:22 +0900

    Feat: Updating the table

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,8 +1,21 @@
 import Search from '@/app/ui/search';
+import InvoicesTable from '@/app/ui/invoices/table';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
+import { Suspense } from 'react';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 
-export default async function Page() {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query ?? '';
+  const currentPage = Number(searchParams?.page ?? '1');
+
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
@@ -12,6 +25,9 @@ export default async function Page() {
         <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <InvoicesTable query={query} currentPage={currentPage} />
+      </Suspense>
       <div className="mt-5 flex w-full justify-center"></div>
     </div>
   );
