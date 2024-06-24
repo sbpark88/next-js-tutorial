c6aa04055523bd7b41fe140352e9e60a4b6d001a
sbpark88 <saebyulpark88@gmail.com>
2024-06-24 21:52:07 +0900

    Refactor: Metadata can provide template

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,10 @@ import { inter } from '@/app/ui/fonts';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Acme Dashboard',
+  title: {
+    default: 'Acme Dashboard',
+    template: '%s | Acme Dashboard',
+  },
   description: 'The official Next.js Course Dashboard, built with App Router.',
   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
 };
